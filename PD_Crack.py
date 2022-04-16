9e6a8408dac96b4dbb54b949711a46b8a1d244ec
LuYee <aulaul920106@gmail.com>
2022-04-16 19:56:23 +0800

    Update PD_Crack.py

--- PD_Crack.py
+++ PD_Crack.py
@@ -20,33 +20,35 @@ print("==================================")
 for f in files:
   # 產生檔案的絕對路徑
   fullpath = join(mypath, f)
-
   if isdir(fullpath):
+    # 添加虛擬機到陣列和變數
     vm.append(f[0:-4])
+    os.environ['vm'+str(c)] = str(f[0:-4])
     print("虛擬機#"+str(c)+"：", vm[c])
   c=c+1
 
 print("==================================")
 
+# 儲存編號變數
 ch = int(input("請選擇要開啟的虛擬機編號#:"))
-os.environ["vmname"]="'"+vm[ch]+"'"
+# os.environ["vmname"]="'"+vm[ch]+"'"
 
-
-
+# 儲存密碼變數
 passwd = str(input("請輸入密碼："))  
 os.environ['passwd']=str(passwd)
 
+exploit = "echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start $vm" + str(ch)
+os.system(exploit)
 
 
-os.system("bash -c 'echo 正在開啟 $vmname'")
-
-if ch == 1:
-  os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Ubuntu＿20.04.2＿ARM64'")
-elif ch == 2:
-  os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Kali＿Linux'")
-elif ch == 3:
-  os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Windows＿11'")
+# os.system("bash -c 'echo 正在開啟 $vmname'")
+# if ch == 1:
+#   os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Ubuntu＿20.04.2＿ARM64'")
+# elif ch == 2:
+#   os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Kali＿Linux'")
+# elif ch == 3:
+#   os.system("echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start 'Windows＿11'")
 
 os.system("echo $passwd | sudo -S systemsetup -setnetworktimeserver time.apple.com")
 
-os.system("exit")
+# os.system("exit")
