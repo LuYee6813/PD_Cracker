5f96bbbddbfe2e5bca04ac6c1ada30e856863bc6
LuYee <aulaul920106@gmail.com>
2022-04-17 18:26:49 +0800

    Update PD_Crack.py

--- PD_Crack.py
+++ PD_Crack.py
@@ -4,19 +4,15 @@ import os
 from os import listdir
 from os.path import isdir, join
 
-# get current user directory
 mypath = os.environ.get('HOME')+"/Parallels"
-
-# get all data on mypath
 files = listdir(mypath)
-
-# virtual machine list
 vm=[0]
 
 c=0
 print("==================================")
 print("Parallels Desktop 繞過神器")
 print("==================================")
+os.system("open /Applications/Parallels\ Desktop.app")
 for f in files:
   # 產生檔案的絕對路徑
   fullpath = join(mypath, f)
@@ -26,17 +22,16 @@ for f in files:
     os.environ['vm'+str(c)] = str(f[0:-4])
     print("虛擬機#"+str(c)+"：", vm[c])
   c=c+1
-
 print("==================================")
 
 # 儲存編號變數
 ch = int(input("請選擇要開啟的虛擬機編號#:"))
-# os.environ["vmname"]="'"+vm[ch]+"'"
-
 # 儲存密碼變數
 passwd = str(input("請輸入密碼："))  
 os.environ['passwd']=str(passwd)
 
+
+# 漏洞利用
 exploit = "echo $passwd | sudo -S date 010100002018 && /usr/local/bin/prlctl start $vm" + str(ch)
 os.system(exploit)
 os.system("echo $passwd | sudo -S systemsetup -setnetworktimeserver time.apple.com")
