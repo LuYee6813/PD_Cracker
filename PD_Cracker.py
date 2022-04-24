c424078a5c15951470527b91c880b5009edf6044
LuYee <aulaul920106@gmail.com>
2022-04-25 05:55:47 +0800

    Update PD_Cracker.py

--- PD_Cracker.py
+++ PD_Cracker.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from ctypes import sizeof
 import os
 from os import listdir
 from os.path import isdir, join
 
 def start_vm():
-    mypath = os.environ.get('HOME')+"/Parallels"
-    files = listdir(mypath)
-    vm = [0]
-    c = 0
-    print("""
+    if (os.system("netstat | grep 'Parallels'") == 256):
+        try:
+            os.system("open /Applications/Parallels\ Desktop.app")
+            start_vm()
+        except:
+            print("請重新啟動！檢查是否已開啟Parallels Desktop")
+    else:
+        mypath = os.environ.get('HOME')+"/Parallels"
+        files = listdir(mypath)
+        vm = [0]
+        c = 0
+        print("""
  _______  ______             ______                        __                     
 |_   __ \|_   _ `.         .' ___  |                      [  |  _                 
   | |__) | | | `. \       / .'   \_| _ .--.  ,--.   .---.  | | / ] .---.  _ .--.  
   |  ___/  | |  | |       | |       [ `/'`\]`'_\ : / /'`\] | '' < / /__\\[ `/'`\] 
  _| |_    _| |_.' /_______\ `.___.'\ | |    // | |,| \__.  | |`\ \| \__., | |     
-|_____|  |______.'|_______|`.____ .'[___]   \'-;__/'.___.'[__|  \_]'.__.'[___]    
-    """)
+|_____|  |______.'|_______|`.____ .'[___]   \'-;__/'.___.'[__|  \_]'.__.'[___] 
+        """)
 
-    for fs in files:
-        # 產生檔案的絕對路徑
-        fullpath = join(mypath, fs)
-        if isdir(fullpath):
-            vm.append(fs[0:-4])
-            c+=1
-            print("虛擬機[%d]: %s"%(c,vm[c]))
-    print("==================================")
-    
-    try:
-        ch = int(input("請選擇要開啟的虛擬機編號： "))
-        f = open("password.txt",'r')
-        passwd = f.read()
-        exploit = "echo %s | sudo -S date 010100002018 && /usr/local/bin/prlctl start '%s'" % (passwd,vm[ch])  
-        os.system(exploit)
-        time_up = "echo %s | sudo -S systemsetup -setnetworktimeserver time.apple.com" % (passwd)
-        os.system(time_up)
-    except:
-        print("請選擇編號內的虛擬機")
-        start_vm()
+        for fs in files:
+            # 產生檔案的絕對路徑
+            fullpath = join(mypath, fs)
+            if isdir(fullpath):
+                vm.append(fs[0:-4])
+                c+=1
+                print("虛擬機[%d]: %s"%(c,vm[c]))
+        print("==================================")
+        
+        try:
+            ch = int(input("請選擇要開啟的虛擬機編號： "))
+            f = open("password.txt",'r')
+            passwd = f.read()
+            exploit = "echo %s | sudo -S date 010100002018 && /usr/local/bin/prlctl start '%s'" % (passwd,vm[ch])  
+            os.system(exploit)
+            time_up = "echo %s | sudo -S systemsetup -setnetworktimeserver time.apple.com" % (passwd)
+            os.system(time_up)
+        except:
+            print("請選擇編號內的虛擬機")
 
 
 if os.path.isfile("password.txt"):
@@ -50,8 +55,7 @@ else:
   | |__) | | | `. \       / .'   \_| _ .--.  ,--.   .---.  | | / ] .---.  _ .--.  
   |  ___/  | |  | |       | |       [ `/'`\]`'_\ : / /'`\] | '' < / /__\\[ `/'`\] 
  _| |_    _| |_.' /_______\ `.___.'\ | |    // | |,| \__.  | |`\ \| \__., | |     
-|_____|  |______.'|_______|`.____ .'[___]   \'-;__/'.___.'[__|  \_]'.__.'[___]    
-                                                                                     
+|_____|  |______.'|_______|`.____ .'[___]   \'-;__/'.___.'[__|  \_]'.__.'[___]                                                                      
     """)
     passwd = str(input("第一次使用請輸入密碼:"))
     f = open('password.txt', 'w')
